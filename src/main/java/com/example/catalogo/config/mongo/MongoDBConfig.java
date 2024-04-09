666659c1fcd9f20bac4f27af208d98cf97528e3d
Pablo Diamante <pablo-dgs1@hotmail.com>
2024-04-09 15:01:05 -0300

    create product e get products

--- src/main/java/com/example/catalogo/config/mongo/MongoDBConfig.java
+++ src/main/java/com/example/catalogo/config/mongo/MongoDBConfig.java
@@ -6,6 +6,12 @@ import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
+// import com.example.catalogo.domain.category.Category;
+// import org.springframework.boot.CommandLineRunner;
+// import com.example.catalogo.domain.product.Product;
+// import com.example.catalogo.repositories.CategoryRepository;
+// import com.example.catalogo.repositories.ProductRepository;
+
 @Configuration
 public class MongoDBConfig {
     @Bean
@@ -18,4 +24,12 @@ public class MongoDBConfig {
         return new MongoTemplate(mongoConfigure());
     }
 
+    // @Bean
+    // CommandLineRunner commandLineRunner(CategoryRepository categoryRepository, ProductRepository productRepository) {
+    //     return strings -> {
+    //         System.out.println("MongoDB Configured");
+    //         categoryRepository.save(new Category());
+    //         productRepository.save(new Product("id19283", "title", "descript", "owndsers", 2138));
+    //     };
+    // }
 }
--- src/main/java/com/example/catalogo/domain/product/Product.java
+++ src/main/java/com/example/catalogo/domain/product/Product.java
@@ -3,14 +3,20 @@ package com.example.catalogo.domain.product;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.example.catalogo.domain.category.Category;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Document(collection = "products")
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Product {
     @Id
     private String id;
@@ -18,5 +24,6 @@ public class Product {
     private String description;
     private String ownerId;
     private Integer price;
+    private Category category;
 
 }
