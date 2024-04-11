5f8aa37aedbaf7015cb453f63d283106f232b917
Pablo Diamante <pablo-dgs1@hotmail.com>
2024-04-11 14:46:30 -0300

    removed unused import

--- src/main/java/com/example/catalogo/domain/product/ProductDTO.java
+++ src/main/java/com/example/catalogo/domain/product/ProductDTO.java
@@ -1,6 +1,4 @@
 package com.example.catalogo.domain.product;
 
-import com.example.catalogo.domain.category.Category;
-
 public record ProductDTO(String title, String description, String ownerId, Integer price, String categoryId) {
 }
